d7698e1ca8eb3045dee489bb41dbdf1623708977
GuiCosta98 <guui.coosta@gmail.com>
2026-01-02 14:31:12 -0300

    teste sonar

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,12 @@
 import React from "react";
+import FuncaoTeste from "./teste"; // Apagar mais tarde. 
 
 
 function Header() {
   return (
     <header className="header">
       <h1 className="header h1">Keeper</h1>
+      <FuncaoTeste/>
     </header>
   );
 }
